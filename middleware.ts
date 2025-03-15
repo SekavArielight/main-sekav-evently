fc8a86818ef003755ff4d3370ebea11d7f54f581
God'swill Sekav Apya <godswillapya@gmail.com>
2025-03-15 23:31:49 +0100

    Further authentication setup

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,19 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  publicRoutes: [
+    "/api",
+    "/events/:id",
+    "/api/webhooks/clerk",
+    "/api/webhooks/stripe",
+    "/api/uploadthing"
+  ],
+  ignoredRoutes: [
+    "/api/webhooks/clerk",
+    "/api/webhooks/stripe",
+    "/api/uploadthing"
+  ]
+});
 
 export const config = {
   matcher: [
